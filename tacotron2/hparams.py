38de9cd8037d2240c15779c842fbf326fa0593ac
tuenguyen <tue.nguyen.2411@gmail.com>
2021-04-01 15:49:00 +0700

    fix print

--- tacotron2/hparams.py
+++ tacotron2/hparams.py
@@ -164,7 +164,7 @@ def create_hparams(hparams_string=None, verbose=False):
             ################################
         # Experiment Parameters        #
         ################################
-        epochs=3,
+        epochs=8,
         iters_per_checkpoint=1000,
         seed=1234,
         dynamic_loss_scaling=True,
